bac3a04673916ad782f0ce3be566870a6b123551
Cole Webster <Cole.Webster@gmail.com>
2022-01-31 14:58:51 -0500

    Added helpers and utils after working on refractor from mini project

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const htmlRoutes = require('./routes/htmlroutes');
-const apiRoutes = require('./routes/apiroutes');
+const api = require('./routes/apiroutes');
+const html = require('./routes/htmlroutes');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,17 +11,27 @@ const PORT = process.env.PORT || 3001;
 // app.use
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
 api.use('/api', api);
+app.use(express.static('public'));
 
 
-// Get notes
+// Get Homepage
+app.get('/notes', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+// Get Notes page
 app.get('/notes', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 });
+
 
 // Wildcard
 app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, './public/index.html'))
 });
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
