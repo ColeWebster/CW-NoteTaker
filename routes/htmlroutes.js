7c0fba3130ba456a2ebf713fad0907638d5a4347
Cole Webster <Cole.Webster@gmail.com>
2022-02-01 20:26:27 -0500

    Updating notes get router

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -3,15 +3,13 @@ const path = require("path");
 const router = require("express").Router();
 
 // Notes
-router.get("./notes", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/notes"))
+router.get("/notes", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-
-
 // Home page - Wildcard
-router.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, "/public/index"))
+router.get("*", (req,res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
 module.exports = router;
