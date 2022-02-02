0ebd2f9a17a86191e720828729e79f4aa80977cf
Cole Webster <Cole.Webster@gmail.com>
2022-02-01 21:42:02 -0500

    Updated server file to include the reference to the notes page which functions nwo

--- routes/htmlroutes.js
+++ routes/htmlroutes.js
@@ -1,15 +1,15 @@
 //Use path to path the express items
-const path = require("path");
-const router = require("express").Router();
+// const path = require("path");
+// const app = require("express").Router();
 
-// Notes
-router.get('/', (req,res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'))
-});
+// // Notes
+// app.get('/', (req,res) => {
+//     res.sendFile(path.join(__dirname, '../public/notes.html'))
+// });
 
-//Wildcard
-router.get('*', (req,res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'))
-  });
+// //Wildcard
+// app.get('*', (req,res) => {
+//     res.sendFile(path.join(__dirname, './public/index.html'))
+//   });
   
-module.exports = router;
+// module.exports = app;
--- server.js
+++ server.js
@@ -1,17 +1,27 @@
 // Import express package
 const express = require("express");
+const path = require("path");
 const apiRoutes = require("./routes/apiroutes");
-const router = require("./routes/htmlroutes") 
+// const router = require("./routes/htmlroutes") 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
-
-
 // app.use borrowed from activity 20
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
-app.use('/htmlRoutes', router);
+app.use('/apiRoutes', apiRoutes);
+
+// Notes
+app.get('/notes', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+//Wildcard
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 // Status to know its live on a certain port and variable as needed from line 7
 app.listen(PORT, () =>
