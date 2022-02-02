3a60e688e5708ff179dc0ef90f2846de7e688847
Cole Webster <Cole.Webster@gmail.com>
2022-02-01 20:15:00 -0500

    Updated delete and post to check functionality

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,16 +1,42 @@
-const store = require("../db/store");
+const express = require("express");
+const {v4: uuidv4} = require('uuid');
+const {readFromFile, writeToFile, readAndAppend} = require('../helpers/fsUtils')
+
 conster router = require("express").Router();
 
 // Get
-router.get("/notes", (req,res) => {
+router.get('/notes', (req,res) => {
+  readFromFile('./db/db.json').then(data => res.json(JSON.parse(data)))
+});
 
-})
 // Post
+router.post('/notes', (req,res) => {
+  const { title, text } = req.body;
 
-
-
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    readAndAppend(newNote, './db/db.json');
+    res.json(`Note succesfully added`);
+  } else {
+    res.error('Error when adding')
+  }
+});
 
 // Delete
+router.delete('/notes', (req,res) => {
+  const noteID = req.params.id;
+  readFromFile('./db/db.json')
+    .then((data) => JSON.parse(data))
+    .then((json) =>{
+      const result = json.filter((title) => title.id !== id);
+      writeToFile('./db/db.json', result);
+      res.json(`Note ${id} was deleted`)
+    });
+});
 
 
 
